1fd95c638722bdf807ea60420526a74e5310cef6
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-20 10:49:07 -0300

    Registering user

--- Backend/src/services/user/CreateUserService.ts
+++ Backend/src/services/user/CreateUserService.ts
@@ -1,3 +1,4 @@
+import prismaClient from '../../prisma'
 
 interface UserRequest{
     name: string;
@@ -6,11 +7,39 @@ interface UserRequest{
 }
 
 class CreateUserService{
-    async execute({name, email, password}: UserRequest){
+    async execute({ name, email, password }: UserRequest){
 
-        console.log(name)
+        // Verificar se ele enviou um email
+        if(!email){
+            throw new Error("Email incorrect")
+        }
 
-        return { name: name }
+        // Verificar se esse email já está cadastrado
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where:{
+                email: email
+            }
+        })
+
+        if(userAlreadyExists){
+            throw new Error("User already exists")
+        }
+
+        const user = await prismaClient.user.create({
+            data:{
+                name: name,
+                email: email,
+                password: password,
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+
+
+        return user;
     }
 }
 
