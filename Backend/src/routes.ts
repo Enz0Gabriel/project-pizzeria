2d7433ed6e121d1b90078ca24b4c0562b3f1f9e7
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-25 14:26:40 -0300

    Listing categories

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -5,6 +5,7 @@ import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -19,5 +20,7 @@ router.get('/me', isAuthenticated, new DetailUserController().handle)
 
 //-- ROTAS CATEGORY
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router };
---
+++ Backend/src/services/category/ListCategoryService.ts
@@ -0,0 +1,18 @@
+import prismaClient from "../../prisma";
+
+class ListCategoryService{
+    async execute(){
+
+        const category = await prismaClient.category.findMany({
+            select:{
+                id: true,
+                name: true,
+            }
+        })
+
+        return category;
+
+    }
+}
+
+export { ListCategoryService }
