a545e1a058867fc34af3a2a1c58b4f9a17344996
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-27 15:41:35 -0300

    Closing an order

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -12,6 +12,7 @@ import { CreateProductController } from './controllers/product/CreateProductCont
 import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 
 import { CreateOrderController } from './controllers/order/CreateOrderController'
+import { RemoveOrderController } from './controllers/order/RemoveOrderController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -42,5 +43,6 @@ router.get('/category/product', isAuthenticated, new ListByCategoryController().
 
 //-- ROTAS ORDER
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
 
 export { router };
