74a4cd90c7fb3e961a81364aefe834c917d9ff76
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-27 15:43:00 -0300

    Closing an order controller and service

---
+++ Backend/src/controllers/order/RemoveOrderController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express'
+import { RemoveOrderService } from '../../services/order/RemoveOrderService'
+
+class RemoveOrderController{
+    async handle(req: Request, res: Response){
+        const order_id = req.query.order_id as string;
+
+        const removeOrder = new RemoveOrderService();
+
+        const order = await removeOrder.execute({
+            order_id
+        });
+
+        return res.json(order);
+
+    }
+}
+
+export { RemoveOrderController }
