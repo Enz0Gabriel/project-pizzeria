05415567fb606716452b303fe2389a516c2aa059
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-27 13:45:52 -0300

    Registering products

--- Backend/src/controllers/product/CreateProductController.ts
+++ Backend/src/controllers/product/CreateProductController.ts
@@ -12,15 +12,13 @@ class CreateProductController{
             throw new Error("error upload file")
         }else{
 
-            const { originalname, filename } = req.file;
-
-            console.log(filename)
+            const { originalname, filename: banner } = req.file;
 
             const product =  await createProductService.execute({
                 name,
                 price,
                 description,
-                banner: '',
+                banner,
                 category_id
             });
     
--- Backend/src/services/product/CreateProductService.ts
+++ Backend/src/services/product/CreateProductService.ts
@@ -11,7 +11,17 @@ interface ProductRequest{
 class CreateProductService{
     async execute({name, price, description, banner, category_id}: ProductRequest){
 
-        return { ok: true }
+        const product = await prismaClient.product.create({
+            data:{
+                name: name,
+                price: price,
+                description: description,
+                banner: banner,
+                category_id: category_id,
+            }
+        })
+
+        return product;
 
     }
 }
