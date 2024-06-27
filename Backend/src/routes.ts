4a6ea6ba9dd92c72b7de99a673fe68d27a4c81f2
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-06-27 14:53:30 -0300

    Filtering products by category

--- Backend/src/routes.ts
+++ Backend/src/routes.ts
@@ -9,6 +9,7 @@ import { CreateCategoryController } from './controllers/category/CreateCategoryC
 import { ListCategoryController } from './controllers/category/ListCategoryController'
 
 import { CreateProductController } from './controllers/product/CreateProductController'
+import { ListByCategoryController } from './controllers/product/ListByCategoryController'
 
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
@@ -35,5 +36,6 @@ router.get('/category', isAuthenticated, new ListCategoryController().handle)
 //-- ROTAS PRODUCT
 router.post('/product', isAuthenticated, upload.single('file'), new CreateProductController().handle)
 
+router.get('/category/product', isAuthenticated, new ListByCategoryController().handle)
 
 export { router };
---
+++ Backend/src/services/product/ListByCategoryService.ts
@@ -0,0 +1,22 @@
+import prismaClient from "../../prisma";
+
+interface ProductRequest{
+    category_id: string;
+}
+
+class ListByCategoryService{
+    async execute({ category_id }: ProductRequest){
+
+        const findByCategory = await prismaClient.product.findMany({
+            where:{
+                category_id: category_id
+            }
+        })
+
+
+        return findByCategory;
+        
+    }
+}
+
+export { ListByCategoryService }
