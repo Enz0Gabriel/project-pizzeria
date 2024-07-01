5ec49fd501746552aec4b1752ffd5d3410396af4
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-01 15:39:14 -0300

    Configuring style

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,4 +1,8 @@
+import '../../styles/globals.scss'
+import { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
       <div>
-         <h1>Sujeito Pizza</h1>  
+         <h1>Sujeito Pizza :)</h1>  
       </div>
   );
 }
