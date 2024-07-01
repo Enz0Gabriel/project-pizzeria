9080af3af8fb2a500828b86c078c009c77ab550c
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-01 15:48:26 -0300

    Creating document

---
+++ frontend/src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript} from 'next/document'
+
+export default function Document(){
+    return(
+        <Html>
+            <Head>
+
+            </Head>
+
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </Html>
+    )
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,14 @@
-
+import Head from 'next/head'
 
 export default function Home() {
   return (
+      <>
+      <Head>
+        <title>SujeitoPizza - Faça seu login</title>
+      </Head>
       <div>
-         <h1>Sujeito Pizza :)</h1>  
+        <h1>Sujeito Pizzaria</h1>
       </div>
+      </>
   );
 }
