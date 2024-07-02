8ef21e2c0eeef2b930073f40c7524d5c8c51ce6b
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-02 17:31:16 -0300

    Creating register page

--- frontend/src/pages/register/index.tsx
+++ frontend/src/pages/register/index.tsx
@@ -1,25 +1,32 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../../styles/home.module.scss';
+import styles from '../../../styles/home.module.scss';
 
-import logoimg from '../../public/logo.svg';
+import logoimg from '../../../public/logo.svg';
 
-import { Input } from '../components/ui/Input/index'
-import { Button } from '../components/ui/Button/index'
+import { Input } from '../../components/ui/Input/index'
+import { Button } from '../../components/ui/Button/index'
 
 import Link from 'next/link';
 
-export default function Home() {
+export default function Register() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu login</title>
+        <title>Faça seu cadastro</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoimg} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.login}>
+            <h1>Criando conta</h1>
+
           <form>
+            <Input
+              placeholder="Digite seu nome"
+              type="text"
+            />
+
             <Input
               placeholder="Digite seu email"
               type="text"
@@ -34,12 +41,12 @@ export default function Home() {
               type="submit"
               loading={false}
             >
-              Cadastrar
+              Acessar
             </Button>
           </form>
         
-          <Link href="/register" legacyBehavior>
-            <a className={styles.text}>Criar Conta</a>
+          <Link href="/" legacyBehavior>
+            <a className={styles.text}>Fazer Login</a>
           </Link>
 
         </div>
