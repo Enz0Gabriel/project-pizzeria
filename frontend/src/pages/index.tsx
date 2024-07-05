f59b6e70e6becda0d119166124fea58589d71ad5
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-05 14:09:16 -0300

    Creating private routes

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -13,6 +13,8 @@ import { AuthContext } from '../contexts/AuthContext'
 
 import Link from 'next/link';
 
+import { canSSRGuest } from '../utils/canSSRGuest'
+
 export default function Home() {
   const { signIn } = useContext(AuthContext)
 
@@ -83,3 +85,11 @@ export default function Home() {
     </>
   );
 }
+
+
+export const getServerSideProps = canSSRGuest(async (ctx) => {
+  
+  return {
+    props: {}
+  }
+})
