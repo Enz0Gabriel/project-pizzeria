46519d1324351cb1ae09c7ceaf0bdc6ba8637645
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-08 13:17:55 -0300

    Private routes 2

---
+++ frontend/src/utils/canSSRAuth.ts
@@ -0,0 +1,40 @@
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
+import { parseCookies, destroyCookie } from 'nookies'
+import { AuthTokenError } from '../services/errors/AuthTokenError'
+
+//função para paginas que só users logados podem ter acesso.
+
+export function canSSRAuth<P>(fn: GetServerSideProps<P>){
+    return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
+        const cookies = parseCookies(ctx);
+
+        const token = cookies['@nextauth.token']
+
+        if(!token){
+            return{
+                redirect:{
+                    destination: '/',
+                    permanent: false,
+                }
+            }
+        }
+
+        try{
+            return await fn(ctx)
+        }catch(err){
+            if(err instanceof AuthTokenError){
+                destroyCookie(ctx, '@nextauth.token')
+
+                return{
+                    redirect:{
+                        destination: '/',
+                        permanent: false,
+                    }
+                }
+
+            }
+        }
+
+    }
+
+}
