078744ff5dfaf2a5336763bda2b871d69636ffcc
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-11 13:58:16 -0300

    Registering category

--- frontend/src/pages/category/index.tsx
+++ frontend/src/pages/category/index.tsx
@@ -3,13 +3,30 @@ import Head from 'next/head'
 import { Header } from '../../components/Header'
 import styles from './style.module.css'
 
+import { setupAPIClient } from '../../services/api'
+import { toast } from 'react-toastify'
+
+import { canSSRAuth } from '../../utils/canSSRAuth'
+
 export default function Category(){
     const [name, setName] = useState('')
 
     async function handleRegister(event: FormEvent){
         event.preventDefault();
 
-        alert("CATEGORIA " + name)
+        if(name === ''){
+            toast.warn('Digite alguma coisa!')
+            return;
+        }
+
+        const apiClient = setupAPIClient();
+        await apiClient.post('/category', {
+            name: name
+        })
+
+        toast.success('Categoria cadastrada com sucesso!')
+        setName('');
+
     }
 
 
@@ -45,3 +62,12 @@ export default function Category(){
         </>
     )
 }
+
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+    return {
+        props: {}
+    }
+
+})
