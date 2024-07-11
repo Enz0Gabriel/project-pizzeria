01b7a9376cc071c36c08f089780bb05f49f4ef70
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-11 14:32:07 -0300

    Product screen

---
+++ frontend/src/pages/product/index.tsx
@@ -0,0 +1,68 @@
+
+import Head from 'next/head';
+import styles from './styles.module.scss'
+import { Header } from '../../components/Header'
+
+import { canSSRAuth } from '../../utils/canSSRAuth'
+
+
+export default function Product(){
+    return(
+        <>
+        <Head>
+            <title>Novo produto - Sujeito Pizzaria</title>
+        </Head>
+        <div>
+            <Header/>
+
+            <main className={styles.container}>
+                <h1>Novo produto</h1>
+
+                <form className={styles.form}>
+
+
+                    <select>
+                        <option>
+                            Bebida
+                        </option>
+                        <option>
+                            Pizzas
+                        </option>
+                    </select>
+
+                    <input 
+                    type="text" 
+                    placeholder="Digite o nome do produto"
+                    className={styles.input}
+                    />
+
+                    <input 
+                    type="text" 
+                    placeholder="Preço do produto"
+                    className={styles.input}
+                    />
+
+                    <textarea 
+                        placeholder="Descreva seu produto..."
+                        className={styles.input}
+                    />
+
+                    <button className={styles.buttonAdd} type="submit">
+                        Cadastrar
+                    </button>
+
+                </form>
+
+            </main>
+
+        </div>
+        </>
+    )
+}
+
+export const getServerSideProps = canSSRAuth (async (ctx) => {
+    
+    return {
+        props: {}
+    }
+})
