b703bef37dd2aa4ffa4b0a2d9aedbad958e0bd71
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-29 11:06:16 -0300

    Login screen

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,10 +1,80 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native'
+import { View,
+          Text,
+           StyleSheet,
+           Image,
+           TextInput,
+           TouchableOpacity,
+           } from 'react-native'
 
 export default function SignIn(){
   return(
-    <View>
-      <Text>Tela de login</Text>
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../../assets/logo.png')}
+      />
+
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Digite seu email"
+          placeholderTextColor="#F0F0F0"
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Digite sua senha"
+          placeholderTextColor="#F0F0F0"
+          style={styles.input}
+          secureTextEntry={true}
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Acessar</Text>
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1d1d2e'
+  },
+  logo:{
+    marginBottom: 18
+  },
+  inputContainer:{
+    width: '95%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 32,
+    paddingHorizontal: 14,
+  },
+  input:{
+    width: '95%',
+    height: 40,
+    backgroundColor:'#101026',
+    margin: 12,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+    color: '#FFF'
+  },
+  button:{
+    width: '95%',
+    height: 40,
+    backgroundColor: '#3FFFA3',
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
+  },
+  buttonText:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#101026'
+  }
+})
