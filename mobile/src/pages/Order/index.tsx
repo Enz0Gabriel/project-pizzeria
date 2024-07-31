e1d7b0359b627556e3a19bbf6cd73c3a0b046ddc
Enzo Gabriel <124924248+Enz0Gabriel@users.noreply.github.com>
2024-07-31 13:40:38 -0300

    Receiving parameters

--- mobile/src/pages/Order/index.tsx
+++ mobile/src/pages/Order/index.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import { View, Text, StyleSheet} from 'react-native';
 
+import { useRoute, RouteProp } from '@react-navigation/native'
+
+
+type RoutesDetailParams = {
+    Order:{
+        number: string | number;
+        order_id: string;
+    }
+}
+
+type OrderRoutesProps = RouteProp<RoutesDetailParams, 'Order'>;
+
 export default function Order(){
+    const route = useRoute<OrderRoutesProps>();
+
     return(
         <View style={styles.container}>
             <Text>Tela order</Text>
+            <Text>
+                {route.params.number}
+            </Text>
         </View>
     )
 }
